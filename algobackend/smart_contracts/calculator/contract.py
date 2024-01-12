328d0247e4e0b2dad4058707fdf101a2d68bbd06
vboxuser <vboxuser@ubuntu.myguest.virtualbox.org>
2024-01-12 11:17:32 +0300

    beaker_dapp

---
+++ algobackend/smart_contracts/calculator/contract.py
@@ -0,0 +1,33 @@
+import beaker as bk
+import pyteal as pt
+
+class MyState:
+    result = bk.GlobalStateValue(pt.TealType.uint64)
+
+app = bk.Application("Calculator", state=MyState())
+
+@app.external
+def add(a: pt.abi.Uint64, b: pt.abi.Uint64, *, output: pt.abi.Uint64) -> pt.Expr:
+    add_result = a.get() + b.get()
+    return pt.Seq(
+        app.state.result.set(add_result),
+        output.set(add_result)
+    )
+
+@app.external
+def minus(a: pt.abi.Uint64, b: pt.abi.Uint64, *, output: pt.abi.Uint64) -> pt.Expr:
+    add_result = a.get() - b.get()
+    return pt.Seq(
+        app.state.result.set(add_result),
+        output.set(add_result)
+    )
+
+@app.external(read_only=True)
+def read_result(*, output: pt.abi.Uint64) -> pt.Expr:
+    result = app.state.result.get()  # Retrieve the result from the state
+    return output.set(result)
+
+
+if __name__ == "__main__":
+    spec = app.build()
+    spec.export("artifacts")
